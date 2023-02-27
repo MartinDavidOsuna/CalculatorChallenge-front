b6174a6b66b45d72c68ddbd74fa40158c5152943
martindavidosuna <martindavidosuna@gmail.com>
2023-02-27 14:25:49 -0700

    v1.9

--- src/main.js
+++ src/main.js
@@ -18,7 +18,8 @@ import Notifications from '@kyvg/vue3-notification'
 
 let app = createApp(App);
 
-app.config.globalProperties.$API_URL = "https://martinosuna.com:8090/api/V1";
+app.config.globalProperties.$API_URL = "http://martinosuna.com:8080/api/V1";
+//app.config.globalProperties.$API_URL = "http://martinosuna.com:8080/api/V1"; FOR HTTP CONFIGURATION
 
 app.config.globalProperties.emitter = mitt();
 
